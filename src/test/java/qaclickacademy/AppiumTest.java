6e79c71e4fb8a6b3bd79dacc95e65b792ddf4f38
aman <aman_singh42053@yahoo.com>
2019-06-22 08:32:15 +0530

    first commit

---
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,18 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+	@Test
+	public void androidApp()
+	{
+		System.out.println("Android");
+	}
+	@Test
+	public void iosApps()
+	{
+		System.out.println("IOS");
+	}
+
+}
---
+++ src/test/java/qaclickacademy/RestAPITest.java
@@ -0,0 +1,17 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class RestAPITest {
+	
+	@Test
+	public void jiraTest()
+	{
+		System.out.println("JIRA");
+	}
+	@Test
+	public void deleteTwitterTest()
+	{
+		System.out.println("Twitter");
+	}
+}
